eaeaa1230d49eb1ab3564786d8d3b46066403312
Phạm Thanh Phong <92080858+PhamThanhPhongPN@users.noreply.github.com>
2024-12-17 20:47:34 +0700

    Add files via upload

---
+++ BT6.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(){
+	int Choice;
+	float Num1,Num2;
+	printf("Hay nhap vao 2 so bat ky :\n");
+	scanf("%f%f",&Num1,&Num2);
+	do{
+		printf("CALCULATOR\n");
+		printf("1. Tinh tong 2 so\n");
+		printf("2. Tinh hieu 2 so\n");
+		printf("3. Tinh tich 2 so\n");
+		printf("4. Tinh thuong 2 so\n");
+		printf("5. Thoat\n");
+		printf("\n");
+		printf("Lua chon cua ban : ");
+		scanf("%d",&Choice);
+		
+		switch (Choice) {
+			case 1:
+				printf("Tong hai so la %.2f\n",Num1 + Num2);
+				break;
+			case 2:
+				printf("Hieu hai so la %.2f\n",Num1 - Num2);
+				break;
+			case 3:
+				printf("Tich hai so la %.2f\n",Num1 * Num2);
+				break;
+			case 4:
+				if (Num2 == 0){
+					printf("Khong the chia cho 0.\n");
+				} else {
+					printf("Thuong hai so la %.2f\n",Num1 / Num2);
+				}
+				break;
+			case 5:
+				break;
+			default:
+				printf("Thong bao khong co so ban da nhap.\n");
+				break;
+		} 
+	} while (Choice != 5);
+	return 0;
+}
